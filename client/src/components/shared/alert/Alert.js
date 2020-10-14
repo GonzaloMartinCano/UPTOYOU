28d6ea6f49b50f8f0b9f7f420dd23b02db598f9b
Zalillo18 <gmcano85@gmail.com>
2020-10-14 16:15:18 +0200

    iconos en alertas

--- client/src/components/shared/alert/Alert.js
+++ client/src/components/shared/alert/Alert.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import Toast from 'react-bootstrap/Toast'
+import { BiErrorCircle } from 'react-icons/bi'
+import { CgCheckO } from 'react-icons/cg'
+
+
 
 
 export default class Alert extends Component {
@@ -21,14 +25,14 @@ export default class Alert extends Component {
                 onClose={() => this.setState({ visible: false }, this.props.resetAlert)} show={this.state.visible} delay={2000} autohide
                 style={{ position: 'fixed', top: 50, right: "20%", width: '50%', backgroundColor: '#c4f5cd' }}
                 >
-                    <Toast.Body>{this.props.text}</Toast.Body>
+                    <Toast.Body><CgCheckO/> {this.props.text}</Toast.Body>
             </Toast>
                 :
-                <Toast
+                <Toast 
                 onClose={() => this.setState({ visible: false }, this.props.resetAlert)} show={this.state.visible} delay={2000} autohide
                 style={{ position: 'fixed', top: 50, right: "20%", width: '50%', backgroundColor: '#966569' }}
                 >
-                    <Toast.Body>{this.props.text}</Toast.Body>
+                    <Toast.Body> <BiErrorCircle/> {this.props.text}</Toast.Body>
             </Toast>
             }  
             
